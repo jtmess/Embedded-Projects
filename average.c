d7671f9aee09ff99253b20789db402477efcf44c
Jake Messner <messner.j@husky.neu.edu>
2016-01-17 13:45:51 -0500

    Uses mean function to average

---
+++ average.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main(int argc, char** argv)
+{
+  int spectranum=0, loopint=0;
+  printf("Enter the number of spectra:\n");
+  scanf("%d",spectranum);
+  for (loopint=0;loopint<spectranum;loopint++)
+  {
+    char x[spectranum];
+    /** prompt user for integration time */
+    /** store integration time
+    /** read data from spectrometer */
+    i
+    x[loopint]=data;
+    y[loopint]=data;
+  }
+  mean functions
+  graph mean functions
+}
